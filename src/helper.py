4d0e8f7cc22226ab10fee46f3b11e2cbd9697f86
vijayasooriyan <svijayasooriyan@gmail.com>
2026-04-12 23:05:57 +0530

    LInkedIn fun finished

--- src/helper.py
+++ src/helper.py
@@ -2,6 +2,7 @@ import fitz  #PymuPDF
 import os
 import dotenv
 from openai import OpenAI
+from apify_client import ApifyClient
 
 load_dotenv()
 
@@ -9,6 +10,7 @@ OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 os.environ["OPENAI_API_KEY"] = OPENAI_API_KEY
 
 client = OpenAI(api_key=OPENAI_API_KEY)
+apify_client = ApifyClient(os.getenv("APIFY_API_TOKEN"))
 
 def extract_text_from_pdf(uploaded_file):
     """
@@ -51,7 +53,19 @@ def ask_openai(prompt,max_tokens=500):
 
 #fetch linkedin and naukri jobs based on search query and location and return the results as a list of dictionaries with keys: title, company, location, description, url
 def fetch_linkedin_jobs(search_query,location="Srilanka", row=60):
-    pass
+    run_input = {
+        "title": search_query,
+        "location": location,
+        "rows": row,
+        "proxy": {
+            "useApifyProxy": True,
+            "apifyProxyGroups": ["RESIDENTIAL"],
+        }
+    }
+
+    run = apify_client.actor("BHzefUZlZRKWxkTck").call(run_input=run_input)
+    jobs = list(apify_client.dataset(run["defaultDatasetId"]).iterate_items())
+    return jobs
 
 def fetch_naukri_jobs(search_query,location="Srilanka", row=60):
     pass
