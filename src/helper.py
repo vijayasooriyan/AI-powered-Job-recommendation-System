cac297a8d2926d48e2420b4996b866513bc561e3
vijayasooriyan <svijayasooriyan@gmail.com>
2026-04-12 22:18:09 +0530

    fisrt

---
+++ src/helper.py
@@ -0,0 +1,16 @@
+import fitz  #PymuPDF
+import os
+import dotenv
+
+
+def extract_text_from_pdf(pdf_path):
+    """
+    Extracts text from a PDF file.
+
+    Args:
+        pdf_path (str): The path to the PDF file.
+
+    Returns:
+        str: The extracted text from the PDF.
+    """
+    text
